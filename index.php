b19e26fc9bf98608f5579f82189a02e9073cf2a3
jowy99 <jowycarreras@gmail.com>
2023-12-01 13:16:53 +0100

    Upload to github

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Joel Arnaud - Under Construction</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="bg-gray-100 dark:bg-gray-800 flex items-center justify-center min-h-screen dark:text-white">
+    <div class="p-4 bg-gray-700 dark:bg-white md:p-8 mx-4 w-full max-w-screen-md rounded-lg shadow-md text-white dark:text-gray-700">
+        <div class="flex items-center justify-center mb-4">
+            <svg class="w-12 h-12 mr-2" fill="yellow" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="12 2 22 21 2 21" stroke-width="2"></polygon>
+                <text x="12" y="17" font-family="Arial" font-size="10" fill="black" text-anchor="middle">
+                    !
+                </text>
+            </svg>
+            <h1 class="text-3xl md:text-4xl font-bold text-center">Website Under Construction</h1>
+            <svg class="w-12 h-12 mr-2" fill="yellow" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="12 2 22 21 2 21" stroke-width="2"></polygon>
+                <text x="12" y="17" font-family="Arial" font-size="10" fill="black" text-anchor="middle">
+                    !
+                </text>
+            </svg>
+        </div>
+        <p class="text-base md:text-lg lg:text-xl text-center md:mb-6">We are working to improve our website. We apologize for any inconvenience this may cause. Please check back soon for updates!</p>
+        <p class="text-base text-center mb-4 md:mb-6">Thank you for your understanding.</p>
+        <p class="text-base text-center font-semibold">
+            <a href="https://www.instagram.com/sagiittarius24/" class="hover:text-yellow-500">Sincerely, Joel Arnaud</a>
+        </p>
+    </div>
+    </body>
+</html>
